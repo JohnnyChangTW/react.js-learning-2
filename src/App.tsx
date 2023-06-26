7dadd86b694b14ef4cbe9f87772f4d0290dbe589
JohnnyChang <johnny840124@gmail.com>
2023-06-27 00:19:03 +0800

    remove MouseEvent and add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokoyo", "London", "Paris"];
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Cities" />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,17 +1,18 @@
-import { MouseEvent } from "react";
 // go to bootstrap.com -> docs -> components -> ListGroup to find template markup code for ListGroup
+
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+}
+
 // <> </> is equal to telling react to use <Fragment> </Fragment>
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokoyo", "London", "Paris"];
-  let selectedIndex = 0;
-  // Event handler
-  const handleClick = (event: MouseEvent) => {
-    console.log(event); // We can use event. to access its properties
-    // console.log("Cliced:", item, "index:", index); // todo: how access item and index here?
-  };
+function ListGroup({ items, heading }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(0); // Hook, tell react that this component could have data or state that change over time
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
 
       {/*Use short circuiting to acheive conditional rendering */}
       {items.length === 0 && <p>No item found</p>}
@@ -25,7 +26,9 @@ function ListGroup() {
                 : "list-group-item"
             }
             key={item}
-            onClick={handleClick}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
           >
             {item}
           </li>
